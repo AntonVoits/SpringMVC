630c3f4665e33d877ae1ebd3196a6a5fb1f67a90
Anton Voitsekhovskii <antonvoits@mail.ru>
2025-11-23 20:39:09 +0300

    complete task PP_2_2_2

---
+++ src/main/java/web/models/Car.java
@@ -0,0 +1,37 @@
+package web.models;
+
+public class Car {
+    int id;
+    String make;
+    String model;
+
+    public Car(int id, String make, String model) {
+        this.id = id;
+        this.make = make;
+        this.model = model;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
